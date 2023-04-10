fe50742f043daae7fc61e74b1de0741e27406be3
Scott <scottjanda@gmail.com>
2023-04-09 20:32:52 -0400

    initial commit

---
+++ CoffeeShopRegistration/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using CoffeeShopRegistration.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoffeeShopRegistration.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SubmitRegistration(UserProfile userProfile)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+
+            //return View("ThankYou");
+            return RedirectToAction("thankYou", userProfile);
+        }
+
+        public IActionResult ThankYou(UserProfile userProfile)
+        {
+            return View(userProfile);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CoffeeShopRegistration/Models/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShopRegistration.Models
+{
+    public class UserProfile
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        [RegularExpression("[a-zA-Z0-9]", ErrorMessage = "Only alpha-numeric characters allowed!")]
+        public string Password { get; set; }
+    }
+}
